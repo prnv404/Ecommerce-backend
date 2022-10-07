43f89251d14fb1edb45d004c550a3ffbcda106db
pranav <pranavofficial404@gmail.com>
2022-10-07 11:33:18 +0530

    product controller

--- controllers/product-controller.js
+++ controllers/product-controller.js
@@ -1,5 +1,7 @@
 const { StatusCodes } = require('http-status-codes')
 const Product = require('../models/product-model')
+const CustomError = require('../errors')
+const { findOne } = require('../models/product-model')
 
 const createProduct = async (req, res) => {
 	req.body.user = req.user.userId
@@ -7,16 +9,37 @@ const createProduct = async (req, res) => {
 	res.status(StatusCodes.CREATED).json(product)
 }
 const getAllProducts = async (req, res) => {
-	res.send('getAllProducts')
+	const products = await Product.find({})
+	res.status(StatusCodes.OK).json({ products, count: products.length })
 }
 const getSingleProduct = async (req, res) => {
-	res.send('getSingleProduct')
+	const { id: productId } = req.params
+
+	const product = await Product.findOne({ _id: productId })
+	if (!product) {
+		throw new CustomError.NotFoundError('No product find ')
+	}
+	res.status(StatusCodes.OK).json(product)
 }
 const updateProduct = async (req, res) => {
-	res.send('updateProduct')
+	const { id: productId } = req.params
+	const product = await Product.findOneAndUpdate({ _id: productId }, req.body, {
+		new: true,
+		runValidators: true,
+	})
+	if (!product) {
+		throw new CustomError.NotFoundError('No product find ')
+	}
+	res.status(StatusCodes.OK).json(product)
 }
 const deleteProduct = async (req, res) => {
-	res.send('deleteProduct')
+	const { id: productId } = req.params
+	const product = await Product.findOne({ _id: productId })
+	if (!product) {
+		throw new CustomError.NotFoundError('No product find ')
+	}
+	await product.remove()
+	res.status(StatusCodes.OK).json({ msg: 'product removed' })
 }
 
 const uploadImage = async (req, res) => {
