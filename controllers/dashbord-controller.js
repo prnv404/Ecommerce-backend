87cdb0d5d2699751cf8bc1c8f968bf2b3f557883
pranav <pranavofficial404@gmail.com>
2022-11-07 12:51:41 +0530

    coupon controller

---
+++ controllers/dashbord-controller.js
@@ -0,0 +1,9 @@
+const { StatusCodes } = require('http-status-codes')
+const { model } = require('mongoose')
+const CustomError = require('../errors')
+
+const createCoupon = async (req, res) => {}
+
+module.exports = {
+	createCoupon,
+}
